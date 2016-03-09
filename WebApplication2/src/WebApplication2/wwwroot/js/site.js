042c090691a8e527913c68c75e80bdeabdf4f83e
gerard patea <gpatea@guildlink.com.au>
2016-03-09 13:19:27 +1000

    after javascript tutorial

---
+++ WebApplication2/src/WebApplication2/wwwroot/js/site.js
@@ -0,0 +1,41 @@
+﻿(function () {
+
+
+
+    //var ele = $("#username");
+    //ele.text("fasi talo");
+
+    //var main = $("#main");
+
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color:#888;"
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sideBarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+
+        $sideBarAndWrapper.toggleClass("hide-sidebar");
+
+        if ($sideBarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show sidebar");
+        }
+        else {
+            $(this).text("hide sidebar");
+        }
+    });
+
+
+
+
+})();
